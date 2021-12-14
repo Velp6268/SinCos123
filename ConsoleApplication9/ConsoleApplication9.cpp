8d85ce7d0d82f0b9b891c8959bbb35834f190e8f
bogor <bogor@DESKTOP-DC6RO5J>
2021-12-14 18:35:09 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double  x, y;
+    cout << "Введите значение: " << endl;
+    cout << "cosn= ";
+    cin >> x;
+    cout << "sinN= ";
+    cin >> y;
+
+    cout << "an= " << (cos(1) / sin(1)) * (cos(1) + cos(2)) / (sin(1) + sin(2)) * (cos(1) + cos(2) + cos(x)) / (sin(1) + sin(2) + sin(y)) << endl;
+
+    system("pause");
+    return 0;
+}
